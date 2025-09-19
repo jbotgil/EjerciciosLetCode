2c26f2f9650616f15cd3959d79ce85f3b1ae6d34
jbotgil <javibotella05@gmail.com>
2025-09-19 18:30:19 +0200

    Ejercicio realizado: 2. Add Two Numbers (Medium)

---
+++ src/main/java/addTwoNumbers/Main.java
@@ -0,0 +1,68 @@
+package addTwoNumbers;
+
+public class Main {
+    /*
+    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
+    and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+    Example 1:
+    Input: l1 = [2,4,3], l2 = [5,6,4]
+    Output: [7,0,8]
+    Explanation: 342 + 465 = 807.
+
+    Example 2:
+    Input: l1 = [0], l2 = [0]
+    Output: [0]
+
+    Example 3:
+    Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+    Output: [8,9,9,9,0,0,0,1]
+    */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0); // nodo inicial auxiliar
+        ListNode actual = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int suma = val1 + val2 + carry;
+            carry = suma / 10;
+
+            actual.next = new ListNode(suma % 10);
+            actual = actual.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        if (carry > 0) {
+            actual.next = new ListNode(carry);
+        }
+
+        return dummy.next; // saltamos el nodo inicial auxiliar
+    }
+
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        // Crear listas de ejemplo
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        //printList(main.addTwoNumbers(l1,l2));
+
+        printList(main.addTwoNumbers(l1,l2));
+    }
+}
